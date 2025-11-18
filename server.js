e1987525b6a982741bd300f3affe1b41f459840e
daniel-ciupek <danter005@o2.pl>
2025-11-18 19:35:57 +0100

    Using the controller in the server code and handling API requests from the client

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const http = require('http');
+const {getQuotes, getQuote, getRandom} = require("./controllers/quoteController")
 
 const PORT = 8080;
-
+const API_CONTENT_TYPE = {"Content-Type": "application/json"};
 const server = http.createServer(async (req, res) => {
     console.log("Request!");
-    res.writeHead(200);
-    res.end("Hello World!");
+
+    if(req.url === "/api/quotes" && req.method === "GET") {
+        let quotes = await getQuotes();
+
+        if(quotes) {
+            res.writeHead(200, API_CONTENT_TYPE);
+        } else {
+            res.writeHead(404, API_CONTENT_TYPE);
+            quotes = {message : `Quotes not found`};
+        }
+
+        res.end(JSON.stringify(quotes));
+
+    }
+
+   // res.writeHead(200);
+   // res.end("Hello World!");
 });
 
 server.listen(PORT);
